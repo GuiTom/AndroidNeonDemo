baea1bfc8bdc933ebe7abdd3a74a4fc6a004be96
Jerry Chen <1806235010@qq.com>
2020-11-17 17:53:51 +0800

    添加注释

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -14,10 +14,10 @@ extern "C" {
 void test()
 {
     int16_t result[8];
-    int8x8_t a = vdup_n_s8(121);
-    int8x8_t b = vdup_n_s8(2);
+    int8x8_t a = vdup_n_s8(121);//将121放到8字节数组中的每一个
+    int8x8_t b = vdup_n_s8(2);//将2放到8字节数组中的每一个
     int16x8_t c;
-    c = vmull_s8(a,b);
+    c = vmull_s8(a,b);//两个数组的每个相同位次的元素相乘，接过放到另一个数组
     vst1q_s16(result,c);
     for(int i=0;i<8;i++){
         LOGD("data[%d] is %d ",i,result[i]);
